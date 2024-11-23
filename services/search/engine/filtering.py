cc1b45019085685071c75a9148cfa48e89390c26
Krystian Bajno <krystian.bajno@gmail.com>
2024-11-23 18:09:12 +0100

    Added Rapid7

--- services/search/engine/filtering.py
+++ services/search/engine/filtering.py
@@ -1,5 +1,4 @@
 def filter_by_severity(vulnerabilities, severities):
     return [
-        vuln for vuln in vulnerabilities 
-        if set(sev['severity'].lower() for sev in vuln.severities).intersection(severities)
+        vuln for vuln in vulnerabilities if set(sev['severity'].lower() for sev in vuln.severities).intersection(severities)
     ]
