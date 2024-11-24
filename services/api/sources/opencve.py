2a731bb8e363c5e8a498d149b0c4bbc979c6d29d
Krystian Bajno <krystian.bajno@gmail.com>
2024-11-24 03:36:49 +0100

    Maintenance - OpenCVE source

--- services/api/sources/opencve.py
+++ services/api/sources/opencve.py
@@ -46,39 +46,44 @@ class OpenCVEAPI(Source):
         rows = table.find_all('tr', class_='cve-header')
         
         for row in rows:
-            cve_id = row.find('td').find('a').text
-            vendor = row.find_all('td')[1].find('a').text
-            products = [prod.text for prod in row.find_all('td')[2].find_all('a')]
-            updated_date = row.find_all('td')[3].text.strip()
+            try:
+                cve_id = row.find('td').find('a').text
+                vendor = row.find_all('td')[1].find('a').text
+                products = [prod.text for prod in row.find_all('td')[2].find_all('a')]
+                updated_date = row.find_all('td')[3].text.strip()
 
-            cvss_text = row.find_all('td')[4].find('span').text.strip()
-            if cvss_text:
-                base_score, base_severity = cvss_text.split(' ')
-            else:
-                base_score, base_severity = None, None
+                cvss_text = row.find_all('td')[4].find('span').text.strip()
+                if cvss_text:
+                    base_score, base_severity = cvss_text.split(' ')
+                else:
+                    base_score, base_severity = None, None
 
 
-            description_row = row.find_next_sibling('tr', class_='cve-summary')
-            description = description_row.find('td').text.strip()
-
-            vulnerabilities.append(VulnerabilityFactory.make(
-                id=cve_id,
-                source=self.__class__.__name__,
-                url=f"https://app.opencve.io/cve/{cve_id}",
-                title=cve_id,
-                base_score=base_score,
-                base_severity=base_severity,
-                description=description,
-                vulnerable_components=list([f"{vendor} {product}" for product in products]),
-                date=updated_date
-            ))
-
+                description_row = row.find_next_sibling('tr', class_='cve-summary')
+                description = description_row.find('td').text.strip()
+                
+                vulnerability = VulnerabilityFactory.make(
+                    id=cve_id,
+                    source=self.__class__.__name__,
+                    url=f"https://app.opencve.io/cve/{cve_id}",
+                    title=cve_id,
+                    base_score=base_score,
+                    base_severity=base_severity,
+                    description=description,
+                    vulnerable_components=list([f"{vendor} {product}" for product in products]),
+                    date=updated_date
+                )
+                            
+                vulnerabilities.append(vulnerability)
+            except:
+                continue
+                        
         return vulnerabilities
 
     def search(self, keywords: List[str], max_results) -> List[Vulnerability]:
         vulnerabilities = []
         response = httpx.get(self.__get_search_url(1, keywords))
-        
+                
         if response.status_code != 200:
             return []
         
@@ -90,7 +95,7 @@ class OpenCVEAPI(Source):
                 page_vulnerabilities = self.__get_vulnerabilities(response.text)
                 vulnerabilities.extend(page_vulnerabilities)
                 current_page = current_page + 1
-            except:
+            except Exception as e:
                 break
 
         return vulnerabilities
